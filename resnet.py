9c9f1a843a867516ca2cdd050744b76f05e325df
Mahesh Choudhary <mrak4123@gmail.com>
2020-03-03 16:10:21 +0530

    Update resnet.py

--- resnet.py
+++ resnet.py
@@ -18,9 +18,9 @@ from keras_frcnn.FixedBatchNormalization import FixedBatchNormalization
 
 def get_weight_path():
     if K.image_data_format() == 'th':
-        return 'resnet50_weights_th_dim_ordering_th_kernels_notop.h5'
+        return 'resnet50_weights_th_data_format_th_kernels_notop.h5'
     else:
-        return 'resnet50_weights_tf_dim_ordering_tf_kernels.h5'
+        return 'resnet50_weights_tf_data_format_tf_kernels.h5'
 
 def get_img_output_length(width, height):
     def get_output_length(input_length):
